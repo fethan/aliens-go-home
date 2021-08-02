124e25cb76abdf8b3d8a253e06ab8cd874c7e266
fethan <fethan.us@gmail.com>
2021-08-02 16:27:13 +0300

    first canvas added

---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+const initialState = {
+    message: 'Its easy to integrate React and Redux, isnt it?',
+  }
+  
+  function reducer(state = initialState) {
+    return state;
+  }
+  
+  export default reducer;
