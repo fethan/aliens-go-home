649a90349b98532925d209c00339d370dab6e29c
fethan <fethan.us@gmail.com>
2021-08-03 10:54:37 +0300

    flying object added

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -6,6 +6,8 @@ import CannonBase from './CannonBase';
 import CannonPipe from './CannonPipe';
 import CannonBall from './CannonBall';
 import CurrentScore from './CurrentScore';
+import FlyingObject from './FlyingObject';
+
 
 const Canvas = (props) => {
     const viewBox = [window.innerWidth / -2, 100 - window.innerHeight, window.innerWidth, window.innerHeight];
@@ -18,17 +20,19 @@ const Canvas = (props) => {
             viewBox={viewBox}
         >
 
-        <defs>
-            <filter id="shadow">
-                <feDropShadow dx="1" dy="1" stdDeviation="2" />
-            </filter>
-        </defs>
+            <defs>
+                <filter id="shadow">
+                    <feDropShadow dx="1" dy="1" stdDeviation="2" />
+                </filter>
+            </defs>
             <Sky />
             <Ground />
             <CannonPipe rotation={props.angle} />
             <CannonBase />
             <CannonBall position={{ x: 0, y: -100 }} />
             <CurrentScore score={15} />
+            <FlyingObject position={{ x: -150, y: -300 }} />
+            <FlyingObject position={{ x: 150, y: -300 }} />
         </svg>
     );
 };
